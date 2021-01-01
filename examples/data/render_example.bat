12e63e71243a3fd0f90a953626b0c98c95687aa0
Jiatao Gu <jgu@fb.com>
2021-01-01 13:45:25 -0500

    add copyright headers

--- examples/data/render_example.bat
+++ examples/data/render_example.bat
@@ -1,3 +1,8 @@
+:: Copyright (c) Facebook, Inc. and its affiliates.
+::
+:: This source code is licensed under the MIT license found in the
+:: LICENSE file in the root directory of this source tree.
+
 set BLENDER="C:\\Users\\jgu\\Downloads\\bunny01.blend"
 set codepath="\\wsl$\\Ubuntu\\home\\jgu\\work\\NSVF"
 set OUTPUT="C:\\Users\\jgu\\Downloads\\bunny"
